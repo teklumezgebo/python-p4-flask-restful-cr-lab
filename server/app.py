04353f898802ad69615d32e405abb530b0e97529
Teklu Mezgebo <mezge002@umn.edu>
2024-01-29 22:24:46 -0600

    index route, show route, and post routes created along with methods to handle each route

--- server/app.py
+++ server/app.py
@@ -17,11 +17,53 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    
+    def get(self):
+
+        response_list_dict = [plant.to_dict() for plant in Plant.query.all()]
+
+        response = make_response(
+            response_list_dict,
+            200,   
+        )
+
+        return response
+    
+    def post(self):
+
+        new_record = Plant(
+            name=request.form["name"],
+            image=request.form["image"],
+            price=request.form["price"]
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        response_dict = new_record.to_dict()
+
+        response = make_response(
+            response_dict,
+            201,
+        )
+
+        return response
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    def get(self, id):
+
+        response_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
         
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
